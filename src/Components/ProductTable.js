5692fac386eca39a710189d884eea5e0d183d63b
ethanzack <37124641+ethanzack@users.noreply.github.com>
2019-03-17 17:48:42 -0500

    filter by sizes

--- src/Components/ProductTable.js
+++ src/Components/ProductTable.js
@@ -11,11 +11,14 @@ class ProductTable extends Component {
      super(props);
      this.state = {
        productsInCart: [],
+       visibleProducts: [],
+       colors: {"XS": "primary", "S": "primary", "M": "primary", "L": "primary", "XL": "primary"}
      };
 
      this.addToCart = this.addToCart.bind(this);
      this.clearCart = this.clearCart.bind(this);
      this.remove = this.remove.bind(this);
+     this.selectFilter = this.selectFilter.bind(this);
    }
 
    addToCart(product) {
@@ -28,6 +31,26 @@ class ProductTable extends Component {
      this.setState({productsInCart: []})
    }
 
+   componentWillMount(){
+     let selectedSizes = []
+     let newArray = []
+     for(var key in this.state.colors){
+       if(this.state.colors[key] == "primary"){
+         selectedSizes.push(key)
+       }
+     }
+     this.props.products.forEach((prod) => {
+       selectedSizes.map((size)=>{
+       if(prod.availableSizes.includes(size) && !newArray.includes(prod)){
+         newArray.push(prod)
+       }
+      }
+      )
+
+     })
+     this.setState({visibleProducts: newArray})
+   }
+
    remove(prod) {
      let newArray = []
      for(let i = 0; i < this.state.productsInCart.length; i++){
@@ -38,13 +61,47 @@ class ProductTable extends Component {
      this.setState({productsInCart: newArray})
    }
 
+
+   selectFilter(color){
+     this.setState({visibleProducts: []})
+     let newDict = this.state.colors
+     if(newDict[color] == "secondary"){newDict[color] = "primary";}
+     else{newDict[color] = "secondary"}
+     let selectedSizes = []
+     let newArray = []
+     for(var key in this.state.colors){
+       if(this.state.colors[key] == "primary"){
+         selectedSizes.push(key)
+       }
+     }
+     this.props.products.forEach((prod) => {
+       selectedSizes.map((size)=>{
+       if(prod.availableSizes.includes(size) && !newArray.includes(prod)){
+         newArray.push(prod)
+       }
+      }
+      )
+
+     })
+     this.setState({visibleProducts: newArray})
+     this.setState({colors: newDict})
+
+   }
+
   render() {
     return (
-      <div>
+      <div style ={{alignItems: 'center'}}>
       <Cart removeItem = {this.remove} cartProds = {this.state.productsInCart} clear = {this.clearCart}/>
+        <Row style = {{display: 'flex', justifyContent: 'center'}}>
+          <Button onClick={() => this.selectFilter("XS")} color = {this.state.colors["XS"]} style = {{margin: '10px'}}> XS </Button>
+          <Button onClick={() => this.selectFilter("S")} color = {this.state.colors["S"]} style = {{margin: '10px'}}> S </Button>
+          <Button onClick={() => this.selectFilter("M")} color = {this.state.colors["M"]} style = {{margin: '10px'}}> M </Button>
+          <Button onClick={() => this.selectFilter("L")} color = {this.state.colors["L"]} style = {{margin: '10px'}}> L </Button>
+          <Button onClick={() => this.selectFilter("XL")} color = {this.state.colors["XL"]} style = {{margin: '10px'}}> XL </Button>
+        </Row>
       <Container>
       <Row>
-        {this.props.products.map(function(prod, i){
+        {this.state.visibleProducts.map(function(prod, i){
           return <Col><Product prod = {prod} add = {this.addToCart}/></Col>
         },this)
        }
